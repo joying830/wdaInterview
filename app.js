345383ab5bdd93dda155e0f2aecf66f8c5674cc3
joying830 <k224100@gmail.com>
2025-09-05 15:22:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ---------- CONFIGURE THIS URL to your Apps Script JSON endpoint ----------
-const API_URL = 'https://script.google.com/macros/s/AKfycbwCJ1VyijWnmwAngiXAMzHS3LXNewXWqPdl8bZLAc31ZK9PCtpmH0DbZf20LN3JGOWk/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzjLuPn22PMOv6QyCBsYjEadk5Z0K4uO5EhrKAkQqA6dPo7htcPiQ_xlRzOACLCBgyo/exec';
 
 // 狀態變數
 let isFirstLoad = true;
